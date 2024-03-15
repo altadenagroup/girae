a7ccec4e9e81e9aea72960d709ff4de4a5123844
lyricalsoul <lyricalsoul@proton.me>
2024-03-14 23:57:42 -0300

    yay

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -3,10 +3,13 @@ import { BotContext } from "../types/context.js"
 export default async (ctx: BotContext) => {
     const metadata = await _brklyn.getDittoMetadata()
 
-    const text = `🏓 Ping! ${Math.ceil((Date.now() / 1000) - ctx.message?.date!)}s do Telegram ao bot.
+    const text = `🏓 Ping! ${Math.ceil((Date.now() / 1000) - ctx.message?.date!)}s de delay entre o Telegram ao bot.
 
 <b>Status dos serviços</b>
 ${metadata ? '🟢' : '🔴'} Gerador de imagens: ${metadata ? `<b>ONLINE</b> <i>(ditto v${metadata.version}.0, codenome ${metadata.name})</i>` : 'OFFLINE'}
+
+<b>Geral</b>
+Instância: <code>${process.env.INSTANCE_NAME || 'Lilin'}</code>
     `
 
     return ctx.replyWithHTML(text)
