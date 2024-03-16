95ab7d83b35bd1b78e9170e16896c5d6198a4cf0
lyricalsoul <lyricalsoul@proton.me>
2024-03-16 13:46:17 -0300

    add experimental cards cmd

--- src/commands/cards.ts
+++ src/commands/cards.ts
@@ -1,7 +1,20 @@
 import { BotContext } from '../types/context.js'
 
 export default async (ctx: BotContext) => {
-  return ctx.reply('mc tha esta fazendo isso shhhh')
+  return ctx.reply('Para ver sua coleção...', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: '⭕ Clique aqui',
+            web_app: {
+              url: 'https://girae-web-app-dev.altadena.space/cards',
+            }
+          }
+        ]
+      ]
+    }
+  })
   return ctx.scene.enter('SHOW_USER_CARDS')
 }
 
