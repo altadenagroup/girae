51da216cde2edf34ec0f82aae940e87d59da20fe
renan martins <lyricalsoul@proton.me>
2024-03-16 09:54:42 -0400

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,18 +1,2 @@
 // this file exists because node is retarded
-import * as Sentry from '@sentry/node'
-
-Sentry.init({
-      dsn: process.env.SENTRY_DSN,
-      integrations: [
-        nodeProfilingIntegration(),
-        new Sentry.Integrations.Prisma({ client: this.db }),
-        Sentry.anrIntegration({ captureStackTrace: true })
-      ],
-      tracesSampleRate: 1.0,
-      _experiments: {
-        metricsAggregator: true,
-      }
-    })
-  
-
 import './dist/src/index.js'
