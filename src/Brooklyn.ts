6dbb48a0174ad26777f265d749534b966426a085
renan martins <lyricalsoul@proton.me>
2024-03-16 09:55:43 -0400

    Update Brooklyn.ts

--- src/Brooklyn.ts
+++ src/Brooklyn.ts
@@ -58,6 +58,7 @@ export default class Brooklyn extends Client {
 
     this.setUpCDN()
     this.setUpExitHandler()
+    this.setUpSentry()
   }
 
   private setUpCDN() {
@@ -131,7 +132,7 @@ export default class Brooklyn extends Client {
         new Sentry.Integrations.Prisma({ client: this.db }),
         Sentry.anrIntegration({ captureStackTrace: true })
       ],
-      tracesSampleRate: 0.5,
+      tracesSampleRate: 1.0,
       profilesSampleRate: 1.0,
       _experiments: {
         metricsAggregator: true,
