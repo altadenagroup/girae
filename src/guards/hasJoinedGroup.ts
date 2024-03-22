b2af22a7d400ab66e5ff4bb402c1835a9b5bdca9
renan martins <lyricalsoul@proton.me>
2024-03-21 23:41:00 -0400

    Update hasJoinedGroup.ts

--- src/guards/hasJoinedGroup.ts
+++ src/guards/hasJoinedGroup.ts
@@ -8,15 +8,6 @@ export default async (ctx) => {
     await ctx.reply(`Você não está no meu canal de notícias... 😢\nPara poder usar meus comandos, entre já! ${process.env.NEWS_CHANNEL_ID}`)
     return false
   }
-
-  // if it's a dm, return
-  if (ctx.chat?.type === 'private' && !ctx.message?.text?.includes?.('girar')) {
-    return true
-  }
-
-  if (ctx.chat?.id === -1002096118477 || ctx.chat?.id === -1002058397651 || ctx.chat?.id === -1001945644138 || ctx.chat?.id === -1001786847999) {
-    return true
-  }
-
-  return false
+  
+  return true
 }
