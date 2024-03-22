3d4ea5dcd0459aaeccff36468b546cf0c777c995
renan martins <lyricalsoul@proton.me>
2024-03-21 22:48:52 -0400

    Update isWhitelistedGroup.ts

--- src/guards/isWhitelistedGroup.ts
+++ src/guards/isWhitelistedGroup.ts
@@ -3,7 +3,10 @@ export default async (ctx) => {
   if (!ctx.message) return
 
   if (process.env.ALLOW_FULL_GLOBAL_USAGE) return true
-  if (ctx.chat?.id === -1002096118477 || ctx.chat?.id === -1001945644138 || ctx.chat?.id === -1001786847999) {
+  if (ctx.chat?.id === -1002096118477 || ctx.chat?.id === -1002058397651 || ctx.chat?.id === -1001945644138 || ctx.chat?.id === -1001786847999) {
+    return true
+  }
+  if (ctx.chat?.type === 'private') {
     return true
   }
 
