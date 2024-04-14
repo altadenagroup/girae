6402ab6d626c2cdb7ea1a495ca8fbf039a06a5b7
lyricalsoul <lyricalsoul@proton.me>
2024-04-14 00:38:48 -0300

    fixx this

--- src/guards/isWhitelistedGroup.ts
+++ src/guards/isWhitelistedGroup.ts
@@ -4,6 +4,15 @@ export default async (ctx: BotContext) => {
   // if this doesn't have a message, return
   if (!ctx.message) return
 
+  const config = await _brklyn.db.groupConfig.findFirst({ where: { groupId: ctx.chat!.id } })
+  if ((config?.disabledCommands?.length || 0) > 0) {
+    // @ts-ignore
+    const commandName = ctx.message.text?.split(' ')[0].replace('/', '').split('@')[0].toLowerCase()
+    if (config?.disabledCommands?.includes(commandName)) {
+      return false
+    }
+  }
+
   if (process.env.ALLOW_FULL_GLOBAL_USAGE) return true
   if (ctx.chat?.id === -1002058397651 || ctx.chat?.id === -1002096118477 || ctx.chat?.id === -1001945644138 || ctx.chat?.id === -1001786847999) {
     return true
@@ -15,15 +24,6 @@ export default async (ctx: BotContext) => {
 
   if (await _brklyn.db.groupDrawLock.findFirst({ where: { groupId: ctx.chat!.id } })) {
     return true
-  }
-
-  const config = await _brklyn.db.groupConfig.findFirst({ where: { groupId: ctx.chat!.id } })
-  if ((config?.disabledCommands?.length || 0) > 0) {
-    // @ts-ignore
-    const commandName = ctx.message.text?.split(' ')[0].replace('/', '').split('@')[0].toLowerCase()
-    if (config?.disabledCommands?.includes(commandName)) {
-      return false
-    }
   }
 
   // if this is the beta bot and it's a dm, return true
