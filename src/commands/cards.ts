7d2cd513881850044e98785caa430f76bc065ad7
lyricalsoul <lyricalsoul@proton.me>
2024-05-25 15:50:58 -0300

    true story

--- src/commands/cards.ts
+++ src/commands/cards.ts
@@ -1,10 +1,13 @@
 import { BotContext } from '../types/context.js'
 import { getMentionedTgUser, mentionUser } from '../utilities/telegram.js'
 
+const ROOT_APP_URL = process.env.CARDS_APP_URL || 'https://girae-web-app-dev.altadena.space/cards'
+const TG_APP_URL = process.env.TG_CARDS_APP_URL || 'https://t.me/giraebot/giraecards'
+
 export default async (ctx: BotContext) => {
   if (ctx.chat!.type !== 'private') {
     const tgUser = await getMentionedTgUser(ctx, ctx.args[0])
-    const url = `https://t.me/giraebot/giraecards?startapp=${tgUser.id}`
+    const url = `${TG_APP_URL}?startapp=${tgUser.id}`
     return ctx.replyWithHTML(`<a href="${url}">Veja todas as cartas e coleções ${ctx.from.id === tgUser.id ? 'suas' : ('de ' + mentionUser(tgUser))} clicando aqui!</a>`)
   }
 
@@ -15,7 +18,7 @@ export default async (ctx: BotContext) => {
           {
             text: '⭕ Clique aqui',
             web_app: {
-              url: 'https://girae-web-app-dev.altadena.space/cards'
+              url: ROOT_APP_URL
             }
           }
         ]
