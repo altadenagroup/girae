e2d23d3e4c6fa77a670681d3c47f6c9b2fc24266
lyricalsoul <lyricalsoul@proton.me>
2024-05-25 14:50:28 -0300

    only a man

--- src/guards/isDeveloper.ts
+++ src/guards/isDeveloper.ts
@@ -1,4 +1,4 @@
 export default (ctx) =>
   process.env.JANET_VERSION
-    ? (ctx.update.message.from.id === 6354439429 || ctx.update.message.from.id === 1889562226)
+    ? ((ctx.update.message.from.id === 6354439429) || (ctx.update.message.from.id === 1889562226))
     : (ctx.update.message.from.id === 1889562226)
