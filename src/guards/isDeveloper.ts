784ee74fbcaf8cbd34db2507b9ced1c737cb0f50
lyricalsoul <lyricalsoul@proton.me>
2024-06-09 09:41:08 -0300

    is developer

--- src/guards/isDeveloper.ts
+++ src/guards/isDeveloper.ts
@@ -1,3 +1,3 @@
 export default (ctx) =>
-  (ctx.update.message.from.id === 6354439429) || (ctx.update.message.from.id === 1889562226) || (ctx.update.message.from.id === 5951890211)
+  (ctx.update.message.from.id === 6354439429) || (ctx.update.message.from.id === 1889562226)
 
