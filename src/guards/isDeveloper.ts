f39780cd26657c9d7ef947e268f74cd93f623c48
lyricalsoul <lyricalsoul@proton.me>
2024-06-10 00:05:20 -0300

    save a few nanoseconds by moving these lines

--- src/guards/isDeveloper.ts
+++ src/guards/isDeveloper.ts
@@ -1,6 +1,6 @@
-export default (ctx) => {
-  let ids = [6354439429, 1889562226]
-  if (!process.env.JANET_VERSION) ids = ids.concat([6657417699, 1381335884])
+let ids = [6354439429, 1889562226]
+if (!process.env.JANET_VERSION) ids = ids.concat([6657417699, 1381335884])
 
+export default (ctx) => {
   return ids.includes(ctx.update.message.from.id)
 }
